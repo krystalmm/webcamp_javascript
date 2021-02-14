106498a74672d184e3c415640300ba668ad76949
krystalmm <taeyeon_aina@live.jp>
2021-02-14 12:59:56 +0900

    [Finish] javascriptの基本文法

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-alert('Hello World!');
+// var hello = 'Hello World';
+// alert(hello);
+
+// var int1 = 1;
+// alert(int1);
+
+// alert(4 + 3);
+
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+
+// if 文
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple) {
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple) {
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+
+// 繰り返し処理（while文）
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+
+// 繰り返し処理（for文）
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+alert('1から10まで足し算した結果は' + num + 'です');
