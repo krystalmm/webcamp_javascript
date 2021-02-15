b884cc162c921d5883d4762469a537b5b748a1d0
krystalmm <taeyeon_aina@live.jp>
2021-02-15 12:38:26 +0900

    [Finish] javascript-7

---
+++ jQuery-Lesson/this-children.js
@@ -0,0 +1,37 @@
+// $(function () {
+//   $('.bg1').on('click', function (){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function (){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function (){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function (){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+
+// 上記をthisを使って書き換える！！
+// $(function (){
+//   $('.box1').on('click', function (){
+//     $(this).slideUp();
+//   });
+// });
+
+
+
+// children
+$(function () {
+  $('button').on('click', function (){
+    $('ul').children().css('color', 'red');
+  });
+});
+
+
+
